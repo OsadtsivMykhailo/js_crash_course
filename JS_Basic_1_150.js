14e4dcd702901157a2ee3408c252433a2cf59906
mykhaylo osadtsiv <osadtsiv.m@gmail.com>
2021-11-01 13:31:55 +0200

    part 2

--- JS_Basic_1_150.js
+++ JS_Basic_1_150.js
@@ -1,5 +1,7 @@
 const d = new Date();
 const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 let currentDay = days[d.getDay()];
-
-console.log(`Today is : ${currentDay}.`);
+let hour = d.toLocaleString('en-US', { hour: 'numeric', hour12: true });
+let minuts = d.getMinutes();
+let seconds = d.getSeconds();
+console.log(`Today is : ${currentDay}.` + `\nCurrent time is : ${hour} : ${minuts} : ${seconds}`);
