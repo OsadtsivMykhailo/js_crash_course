3071a0f1dd403e83ca945161427e0b671a3b3f0d
mykhaylo osadtsiv <osadtsiv.m@gmail.com>
2021-11-01 17:04:31 +0200

    9 from 150

---
+++ JS_Basic_9_150.js
@@ -0,0 +1,16 @@
+//9. Write a JavaScript program to calculate days left until next Christmas.
+let today = new Date();
+let christmasYear = today.getFullYear();
+
+if (today.getMonth() == 11 && today.getDate() > 25) {
+  christmasYear = christmasYear + 1;
+}
+
+let christmasDate = new Date(christmasYear, 11, 25);
+let dayMilliseconds = 1000 * 60 * 60 * 24;
+
+let remainingDays = Math.ceil(
+  (christmasDate.getTime() - today.getTime()) /
+   (dayMilliseconds)
+);
+console.log(remainingDays);
