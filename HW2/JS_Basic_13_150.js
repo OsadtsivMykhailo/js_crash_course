0c7a019d0e5c76c0f6f1d0c274ffb88bc1cd10c9
mykhaylo osadtsiv <osadtsiv.m@gmail.com>
2021-11-02 12:12:36 +0200

    13 from 150

---
+++ HW2/JS_Basic_13_150.js
@@ -0,0 +1,6 @@
+//Write a JavaScript exercise to create a variable using a user-defined name.
+var var_name = "safs";
+var n = 120;
+this[var_name] = n;
+console.log(this[var_name]);
+console.log(var_name);
