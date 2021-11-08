c466425ef83eb5df7dc27d86076046dbdc49b6d0
mykhaylo osadtsiv <osadtsiv.m@gmail.com>
2021-11-08 17:27:27 +0200

    player info

--- HW5-6/functions.js
+++ HW5-6/functions.js
@@ -1,7 +1,30 @@
 let str = window.localStorage.getItem("mainCh");
 console.log(str);
 const mainCharackter = JSON.parse(str);
+function startingInfo2 () {
+    if (typeof mainCharackter['surname'] !== "undefined") {
+    document.getElementById("chName").innerHTML = mainCharackter.name + " "
+    + mainCharackter.surname;
+    } else {
+        document.getElementById("chName").innerHTML = mainCharackter.name;
+    }
+}
+startingInfo2();
+
+const startingInfo1 = () => {
+    document.getElementById("chLvl").innerHTML = `Level: ${mainCharackter.lvl}`;
+}
+startingInfo1();
+
+const startingInfo3 = function() {
+    document.getElementById("chStats").innerHTML = `Tai: ${mainCharackter.tai}    Nin: ${mainCharackter.nin}    Gen: ${mainCharackter.gen}`;
+}
+startingInfo3();
 function startingInfo () {
-    document.getElementById("chName").innerHTML = mainCharackter.name; 
+    mainCharackter.curHP = mainCharackter.hp;
+    mainCharackter.curChakra = mainCharackter.chakra;
+    document.getElementById("chHp").innerHTML = `HP: ${mainCharackter.curHP}/${mainCharackter.hp}`;
+    document.getElementById("chMp").innerHTML = `Chakra: ${mainCharackter.curChakra}/${mainCharackter.chakra}`;
 }
 startingInfo();
+
