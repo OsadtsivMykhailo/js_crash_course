26ef16def4b0232dfedf6e589c9dd125724fe4b9
mykhaylo osadtsiv <osadtsiv.m@gmail.com>
2021-11-12 10:17:43 +0200

    JS_fundamentals_1

---
+++ HW10/JS_Fundamental_1.js
@@ -0,0 +1,12 @@
+//1. Write a JavaScript program to compare two objects to determine
+// if the first one contains equivalent property values to the second one.
+
+function compare (obj1 = {}, obj2 = {}) {
+    return JSON.stringify(obj1) === JSON.stringify(obj2);
+}
+
+console.log(compare({a : 2}, {a : 2}));
+console.log(compare({a : 2, b : 3}, {a : 2}));
+console.log(compare({}, {a : 2}));
+console.log(compare({b : 2}, {a : 2}));
+console.log(compare({a : 3}, {a : 2}));
