433eadb8e87f6a58b24498f07d539c5740130523
mykhaylo osadtsiv <osadtsiv.m@gmail.com>
2021-11-16 14:46:25 +0200

    fix

--- HW11/Pete_TheBaker.js
+++ HW11/Pete_TheBaker.js
@@ -14,7 +14,6 @@
 // cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200}); 
 // // must return 0
 // cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000});
-
 // function cakes(recipe, available) {
 //     let res = Infinity;
 //     let recipeKeysValues = Object.entries(recipe);
@@ -55,6 +54,7 @@ function cakes(obj1, obj2) {
     } else max = 0;
     return max;
 }
+
 console.log(cakes({flour: 500, sugar: 200, eggs: 1}, 
     {flour: 1200, sugar: 1200, eggs: 5, milk: 200}));
 console.log(cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, 
